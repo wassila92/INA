a4118ae7903fbd83649f06a1fbdc2dbbdb0e6f2c
samszo <samuel.szoniecky@univ-paris8.fr>
2018-06-01 14:50:23 +0200

    mise en place Zend

---
+++ api/index.php
@@ -0,0 +1,23 @@
+<?php
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+// Ensure library/ is on include_path
+set_include_path(get_include_path().PATH_SEPARATOR.'/var/www/Zend/library');
+
+/*Zend_Application*/ 
+require_once 'Zend/Application.php';
+///
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap()
+            ->run();
+//
